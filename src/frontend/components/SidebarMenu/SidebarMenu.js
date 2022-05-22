86b7e291bf84df53fde43bb51fae5ef4adcb5a8e
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-05-22 21:09:43 +0530

    feat : created sidebar

---
+++ src/frontend/components/SidebarMenu/SidebarMenu.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import "./SidebarMenu.css";
+
+const SidebarMenu = () => {
+  const optionSelected = ({ isActive }) => {
+    return "sidebar__option" + (isActive ? "--selected" : "") + " flex--row";
+  };
+
+  return (
+    <>
+      <nav className="sidebar__container flex--column primary__font">
+        <NavLink to="/feed" className={optionSelected}>
+          <span className="material-icons sidebar__option-icon">home</span>
+          <span className="sidebar__option-text">Home</span>
+        </NavLink>
+        <NavLink to="/explore" className={optionSelected}>
+          <span className="material-icons sidebar__option-icon">label</span>
+          <span className="sidebar__option-text">Explore</span>
+        </NavLink>
+        <NavLink to="/bookmarks" className={optionSelected}>
+          <span className="material-icons sidebar__option-icon">bookmarks</span>
+          <span className="sidebar__option-text">Bookmarks</span>
+        </NavLink>
+        <NavLink to="/profile" className={optionSelected}>
+          <span className="material-icons sidebar__option-icon">person</span>
+          <span className="sidebar__option-text">Profile</span>
+        </NavLink>
+      </nav>
+    </>
+  );
+};
+
+export { SidebarMenu };
