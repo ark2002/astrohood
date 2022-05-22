072555d0177e29ad41d3297476c63a6ff53131c8
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-05-22 21:10:37 +0530

    feat : connected everything

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import "./App.css";
 import React from "react";
 import { Router } from "./frontend/router/Router";
-import { Footer, Navbar } from "./frontend/components";
+import { Navbar, SidebarMenu } from "./frontend/components";
+import { useLocation } from "react-router-dom";
+
+const sidebarDisplay = (pathname) => {
+  if (
+    pathname === "/explore" ||
+    pathname === "/bookmarks" ||
+    pathname === "/profile" ||
+    pathname === "/feed"
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const flexToggle = (pathname) => {
+  if (!sidebarDisplay(pathname)) {
+    return "flex--row division__container";
+  } else {
+    return "";
+  }
+};
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <Navbar />
-      <Router />
-      <Footer />
+      <div className={flexToggle(location.pathname)}>
+        {!sidebarDisplay(location.pathname) && <SidebarMenu />}
+        <div className="router">
+          <Router />
+        </div>
+      </div>
     </div>
   );
 }
--- src/frontend/screens/LandingScreen/LandingScreen.js
+++ src/frontend/screens/LandingScreen/LandingScreen.js
@@ -1,5 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 const LandingScreen = () => {
-  return <>LandingScreen</>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      LandingScreen
+      <button
+        className="btn btn-color--primary btn-font--secondary"
+        onClick={() => navigate("/feed")}
+      >
+        go to App
+      </button>
+    </>
+  );
 };
 
 export { LandingScreen };
