53081822ab5805ed5b67789c21c551b5ca368ae0
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-06-02 14:21:22 +0530

    feat : feed screen

--- src/frontend/screens/FeedScreen/FeedScreen.js
+++ src/frontend/screens/FeedScreen/FeedScreen.js
@@ -1,5 +1,14 @@
+import { FeedPostList, FeedSortOptions, NewPostCard } from "../../components";
+import "./FeedScreen.css";
+
 const FeedScreen = () => {
-  return <>FeedScreen</>;
+  return (
+    <div className="feedScreen flex--column">
+      <NewPostCard />
+      <FeedSortOptions />
+      <FeedPostList />
+    </div>
+  );
 };
 
 export { FeedScreen };
