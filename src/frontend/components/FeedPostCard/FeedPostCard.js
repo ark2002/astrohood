3ec4aa0009e86f084a11a68dc20457fe28a5e079
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-06-06 20:57:16 +0530

    feat : updated feedPostCard

--- src/frontend/components/FeedPostCard/FeedPostCard.js
+++ src/frontend/components/FeedPostCard/FeedPostCard.js
@@ -1,8 +1,56 @@
 import Moment from "react-moment";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToBookmarksHandler,
+  likeAPostHandler,
+  removeFromBookmarksHandler,
+  unlikeAPostHandler,
+} from "../../slices";
 import "./FeedPostCard.css";
 
 const FeedPostCard = ({ post }) => {
-  const { username, content, comments, createdAt, likes } = post;
+  const dispatch = useDispatch();
+
+  const {
+    _id,
+    username,
+    content,
+    comments,
+    createdAt,
+    likes: { likeCount, likedBy },
+  } = post;
+
+  const { currUser, token } = useSelector((store) => store.auth);
+  const { bookmarks } = useSelector((store) => store.post);
+
+  const getIslikedByCurrUser = () =>
+    likedBy.find((user) => user.username === currUser.username) ? true : false;
+
+  const getIsPostBookmarked = () => bookmarks.includes(_id);
+
+  const isLikedByCurrUser = getIslikedByCurrUser();
+  const isPostBookmarked = getIsPostBookmarked();
+
+  const toggleLikeHandler = async () => {
+    try {
+      isLikedByCurrUser
+        ? await dispatch(unlikeAPostHandler({ id: _id, token }))
+        : await dispatch(likeAPostHandler({ id: _id, token }));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const bookmarkHandler = async () => {
+    try {
+      isPostBookmarked
+        ? await dispatch(removeFromBookmarksHandler({ id: _id, token }))
+        : await dispatch(addToBookmarksHandler({ id: _id, token }));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex--row feedpostcard secondary__font">
       <div className="fd-postcard__aside">
@@ -24,17 +72,47 @@ const FeedPostCard = ({ post }) => {
         </div>
         <div className="fd-postcard__content">{content}</div>
         <div className="fd-postcard__options flex--row">
-          <div className="flex--row option--like">
-            <span className="material-icons option--like">favorite_border</span>
-            <p>{likes.likeCount}</p>
+          <div
+            className={`flex--row option--like ${
+              isLikedByCurrUser ? "post__liked" : ""
+            }`}
+          >
+            {isLikedByCurrUser ? (
+              <span
+                className="material-icons option--like"
+                onClick={() => toggleLikeHandler()}
+              >
+                favorite
+              </span>
+            ) : (
+              <span
+                className="material-icons option--like"
+                onClick={() => toggleLikeHandler()}
+              >
+                favorite_border
+              </span>
+            )}
+            <p>{likeCount}</p>
           </div>
           <div className="flex--row option--comment">
             <span className="material-icons">chat_bubble_outline</span>
             <p>{comments.length}</p>
           </div>
-          <span className="material-icons option--bookmark">
-            bookmark_border
-          </span>
+          {isPostBookmarked ? (
+            <span
+              className="material-icons post__bookmarked"
+              onClick={() => bookmarkHandler()}
+            >
+              bookmark
+            </span>
+          ) : (
+            <span
+              className="material-icons option--bookmark"
+              onClick={() => bookmarkHandler()}
+            >
+              bookmark_border
+            </span>
+          )}
           <span className="material-icons option--share">ios_share</span>
         </div>
       </div>
