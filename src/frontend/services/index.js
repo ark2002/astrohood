3e8508deed909d7cbd2600803e95e3f4eab3b45e
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-06-21 22:20:14 +0530

    feat : Connected everything

--- src/frontend/services/index.js
+++ src/frontend/services/index.js
@@ -6,3 +6,14 @@ export {
   editPostService,
 } from "./post-services";
 export { likeAPostService, unlikeAPostService } from "./likes-services";
+export {
+  createACommentService,
+  deleteACommentService,
+  editACommentService,
+  getCommentsService,
+} from "./comment-services";
+export {
+  addToBookmarksService,
+  getBookmarkedService,
+  removeFromBookmarksService,
+} from "./bookmark-services";
