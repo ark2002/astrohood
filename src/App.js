39b47ae140fc1bc687b013951508ad34e7e970e4
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-07-01 22:34:27 +0530

    refac : App

--- src/App.js
+++ src/App.js
@@ -1,60 +1,65 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Router } from "./frontend/router/Router";
 import { Navbar, SidebarMenu, Suggestions } from "./frontend/components";
 import { matchPath, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
-const sidebarDisplay = (pathname) => {
-  const isMatch =
-    matchPath(
-      {
-        path: "/post/:postId",
-        exact: true,
-        strict: true,
-      },
-      pathname
-    ) ||
-    matchPath(
-      {
-        path: "/user/:userId",
-        exact: true,
-        strict: true,
-      },
-      pathname
-    )
-      ? true
-      : false;
-
-  const allowSidebar = ["/explore", "/bookmarks", "/profile", "/feed", "/post"];
-
-  if (allowSidebar.includes(pathname) || isMatch) {
-    return false;
-  } else {
-    return true;
-  }
-};
-
-const flexToggle = (pathname) => {
-  if (!sidebarDisplay(pathname)) {
-    return "flex--row division__container";
-  } else {
-    return "";
-  }
-};
-
 function App() {
   const location = useLocation();
+  const [display, setDisplay] = useState(false);
+
+  const sidebarDisplay = (pathname) => {
+    const isMatch =
+      matchPath(
+        {
+          path: "/post/:postId",
+          exact: true,
+          strict: true,
+        },
+        pathname
+      ) ||
+      matchPath(
+        {
+          path: "/user/:userId",
+          exact: true,
+          strict: true,
+        },
+        pathname
+      )
+        ? true
+        : false;
+
+    const allowSidebar = ["/explore", "/bookmarks", "/profile", "/", "/post"];
+
+    if (allowSidebar.includes(pathname) || isMatch) {
+      setDisplay(true);
+    } else {
+      setDisplay(false);
+    }
+  };
+
+  useEffect(() => {
+    sidebarDisplay(location.pathname);
+    console.log("effect");
+  }, [location.pathname]);
+
   return (
     <div className="App">
       <Navbar />
-      <div className={flexToggle(location.pathname)}>
-        {!sidebarDisplay(location.pathname) && <SidebarMenu />}
+      {display ? (
+        <div className="flex--row division__container">
+          <SidebarMenu />
+          <div className="router">
+            <Router />
+          </div>
+          <Suggestions />
+        </div>
+      ) : (
         <div className="router">
           <Router />
         </div>
-        {!sidebarDisplay(location.pathname) && <Suggestions />}
-      </div>
+      )}
       <ToastContainer className="toast__style" />
     </div>
   );
