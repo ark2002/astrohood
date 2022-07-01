d391d7d1bec280f0de4f67f66782e522f7ad0852
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-07-01 20:10:02 +0530

    feat : Connected Everything

--- src/store.js
+++ src/store.js
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authReducer, themeReducer, postsReducer } from "./frontend/slices";
+import {
+  authReducer,
+  themeReducer,
+  postsReducer,
+  userReducer,
+} from "./frontend/slices";
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
     auth: authReducer,
     post: postsReducer,
+    user: userReducer,
   },
 });
