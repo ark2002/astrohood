9668581cf91a935f8e404f208ff259b37bc04bec
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-07-01 20:04:24 +0530

    feat : getUnfollowedUsers

---
+++ src/frontend/utils/getUnfollowedUsers.js
@@ -0,0 +1,23 @@
+import { getFollowing } from "./getFollowing";
+
+export const getUnfollowedUsers = (users, username) => {
+  const followingUsers = getFollowing(users, username);
+
+  const unfollowedUsers = users?.filter((listItem) =>
+    username === listItem.username
+      ? false
+      : followingUsers?.find(
+          (followedUser) => followedUser.username === listItem.username
+        )
+      ? false
+      : true
+  );
+
+  console.log(unfollowedUsers);
+
+  if (unfollowedUsers?.length > 3) {
+    return unfollowedUsers.slice(0, 3);
+  }
+
+  return unfollowedUsers;
+};
